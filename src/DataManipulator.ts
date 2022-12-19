82fdaac74bbcc25912bb68a08d0c06f8839f4296
V-Padmaja <aishu2162001@gmail.com>
2022-12-19 14:15:42 +0530

    Update DataManipulator.ts

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,13 +1,13 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  price_abc: 'number',
-  price_def: 'number',
-  ratio: 'number',
-  timestamp: 'Date',
-  upper_bound: 'number',
-  lower_bound: 'number',
-  trigger_alert: 'number | undefined',
+  price_abc: number,
+  price_def: number,
+  ratio: number,
+  timestamp: Date,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined,
 }
 
 
